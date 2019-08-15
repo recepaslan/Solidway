79419d29a5c5f366efd653c6328feb7960daf16e
Recep Aslan <recep.aslan@coupa.com>
2019-08-15 09:39:12 -0400

    SRP Good and Bad

---
+++ EmailNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWay
+{
+    public class EmailNotificationService : INotificationService<Employee>
+    {
+        public void Notify(Employee payload)
+        {
+            //send email
+        }
+    }
+}
---
+++ SRP/SRP_Bad.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWay.SRP
+{
+    public class SRP_Bad
+    {
+        public void CreateEmployee(string firstName, string lastName, string email) {
+
+            if (string.IsNullOrEmpty(firstName))
+                throw new ArgumentException("First name is required");
+
+            if(string.IsNullOrEmpty(lastName))
+                throw new ArgumentException("Last name is required");
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentException("Email is required");
+
+            Employee newEmployee = new Employee();
+            newEmployee.FirstName = firstName;
+            newEmployee.LastName = lastName;
+            newEmployee.Email = email;
+
+            var isCreated = SaveEmployee(newEmployee);
+            if (isCreated)
+                SendEmail(newEmployee);
+
+        }
+
+        public void SendEmail(Employee employee) {
+            //Send welcome email to employee
+        }
+
+
+
+        public bool SaveEmployee(Employee employee) {
+            //Save employee to database
+            return true;
+        }
+    }
+}
