9ad86d161a8e7492078c18237f1d111d18a0c041
Recep Aslan <recep.aslan@coupa.com>
2019-08-15 10:45:16 -0400

    dip good and bad

---
+++ DIP/NotificationService_DIP_Bad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWay.DIP
+{
+    public interface IMessage
+    {
+        void SendMessage();
+    }
+
+    public class Notification
+    {
+        private ICollection<IMessage> _messages;
+        public Notification(ICollection<IMessage> messages)
+        {
+            this._messages = messages;
+        }
+        public void Send()
+        {
+            foreach (var message in _messages)
+            {
+                message.SendMessage();
+            }
+        }
+    }
+}
---
+++ DIP/NotificationService_DIP_Good.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWay.DIP
+{
+    public class Email
+    {
+        public string ToAddress { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public void SendEmail()
+        {
+            //Send email
+        }
+    }
+
+    public class SMS
+    {
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+        public void SendSMS()
+        {
+            //Send sms
+        }
+    }
+    class NotificationService_DIP_Bad
+    {
+
+        private Email _email;
+        private SMS _sms;
+        public NotificationService_DIP_Bad()
+        {
+            _email = new Email();
+            _sms = new SMS();
+        }
+
+        public void Send()
+        {
+            _email.SendEmail();
+            _sms.SendSMS();
+        }
+
+    }
+}
